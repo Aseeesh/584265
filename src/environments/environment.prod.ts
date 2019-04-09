f341949178d271372657cc642c6c057ca873f887
ashish.rijal <ashish.rijal@rigonepal.com>
2019-04-09 16:22:06 +0545

    update heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuYsyDcWh1cQZxKN1RzN_455aIMKwqAlc",
+    authDomain: "aerionapp-b5ff0.firebaseapp.com",
+    databaseURL: "https://aerionapp-b5ff0.firebaseio.com",
+    projectId: "aerionapp-b5ff0",
+    storageBucket: "aerionapp-b5ff0.appspot.com",
+    messagingSenderId: "533344659507"
+  }
 };
